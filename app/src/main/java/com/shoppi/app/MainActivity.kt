18ff29bcf1989014de8e68bdb44a0ca3ae75f386
dpwns1234 <dpwns1234@naver.com>
2022-02-14 14:35:04 +0900

    feat: Complete splash screen

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -2,10 +2,16 @@ package com.shoppi.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "class name"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+
+        Log.d(TAG, "onCreate")
     }
 }
